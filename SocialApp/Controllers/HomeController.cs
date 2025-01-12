c0b7bc5f38821137ad08cd3d18b1a3a85ec5ac13
Sy Dat <datnongsy@gmail.com>
2025-01-12 22:52:00 +0700

    Implement like, comment, report, favorite and CRUD Post

--- SocialApp/Controllers/HomeController.cs
+++ SocialApp/Controllers/HomeController.cs
@@ -22,11 +22,18 @@ namespace SocialApp.Controllers
 
         public async Task<IActionResult> Index()
         {
+            int loggedInUserId = 1;
+
             var allPosts = await _context.Posts
+                .Where(n => (!n.IsPrivate || n.UserId == loggedInUserId) && n.Reports.Count < 5)
                 .Include(n => n.User)
+                .Include(n => n.Likes)
+                .Include(n => n.Favorites)
+                .Include(n => n.Comments).ThenInclude(n => n.User)
+                .Include(n => n.Reports)
                 .OrderByDescending(n => n.DateCreated)
                 .ToListAsync();
-        
+
             return View(allPosts);
         }
 
@@ -52,7 +59,7 @@ namespace SocialApp.Controllers
             {
                 string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
 
-                if(post.Image.ContentType.Contains("image"))
+                if (post.Image.ContentType.Contains("image"))
                 {
                     string rootFolderPathImages = Path.Combine(rootFolderPath, "images");
                     Directory.CreateDirectory(rootFolderPathImages);
@@ -60,7 +67,7 @@ namespace SocialApp.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(post.Image.FileName);
                     string filePath = Path.Combine(rootFolderPathImages, fileName);
 
-                    using(var stream = new FileStream(filePath, FileMode.Create)) 
+                    using (var stream = new FileStream(filePath, FileMode.Create))
                         await post.Image.CopyToAsync(stream);
 
                     // Set the URL to the newPost object
@@ -73,6 +80,153 @@ namespace SocialApp.Controllers
             await _context.SaveChangesAsync();
 
             // Redirect to the index page
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TogglePostLike(PostLikeVM postLikeVM)
+        {
+            int loggedInUser = 1;
+
+            // Check if user has already liked the post
+            var like = await _context.Likes
+                .Where(l => l.PostId == postLikeVM.PostId && l.UserId == loggedInUser)
+                .FirstOrDefaultAsync();
+
+            if (like != null)
+            {
+                _context.Likes.Remove(like);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newLike = new Like()
+                {
+                    PostId = postLikeVM.PostId,
+                    UserId = loggedInUser
+                };
+
+                await _context.Likes.AddAsync(newLike);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPostComment(PostCommentVM postCommentVM)
+        {
+            int loggedInUser = 1;
+
+            // Create a post object
+            var newCommnent = new Comment()
+            {
+                UserId = loggedInUser,
+                PostId = postCommentVM.PostId,
+                Content = postCommentVM.Content,
+                DateCreated = DateTime.UtcNow,
+                DateUpdated = DateTime.UtcNow
+            };
+
+            await _context.Comments.AddAsync(newCommnent);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemovePostComment(RemoveCommentVM removeCommentVM)
+        {
+            var commentDb = await _context.Comments.FirstOrDefaultAsync(c => c.Id == removeCommentVM.CommentId);
+
+            if (commentDb != null)
+            {
+                _context.Comments.Remove(commentDb);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TogglePostFavorite(PostFavoriteVM postFavoriteVM)
+        {
+            int loggedInUser = 1;
+
+            // Check if user has already liked the post
+            var favorite = await _context.Favorites
+                .Where(l => l.PostId == postFavoriteVM.PostId && l.UserId == loggedInUser)
+                .FirstOrDefaultAsync();
+
+            if (favorite != null)
+            {
+                _context.Favorites.Remove(favorite);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newFavorite = new Favorite()
+                {
+                    PostId = postFavoriteVM.PostId,
+                    UserId = loggedInUser
+                };
+
+                await _context.Favorites.AddAsync(newFavorite);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TogglePostVisibility(PostVisibilityVM postVisibilityVM)
+        {
+            int loggedInUser = 1;
+
+            // Check if user has already liked the post
+            var post = await _context.Posts
+                .FirstOrDefaultAsync(l => l.Id == postVisibilityVM.PostId && l.UserId == loggedInUser);
+
+            if (post != null)
+            {
+                post.IsPrivate = !post.IsPrivate;
+                _context.Posts.Update(post);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPostReport(PostReportVM postReportVM)
+        {
+            int loggedInUser = 1;
+
+            // Create a report object
+            var newReport = new Report()
+            {
+                UserId = loggedInUser,
+                PostId = postReportVM.PostId,
+                DateCreated = DateTime.UtcNow
+            };
+
+            await _context.Reports.AddAsync(newReport);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostRemove(PostRemoveVM postRemoveVM)
+        {
+            var postDb = await _context.Posts.FirstOrDefaultAsync(c => c.Id == postRemoveVM.PostId);
+
+            if (postDb != null)
+            {
+                _context.Posts.Remove(postDb);
+                await _context.SaveChangesAsync();
+            }
+
             return RedirectToAction("Index");
         }
     }
